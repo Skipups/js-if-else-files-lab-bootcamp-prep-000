672f2f69e974ec78af780dc090e571e884e612fa
modern-channel-7516 <svetlana.rovinsky@gmail.com>
2019-03-25 02:23:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 var name = "Susan"
 var height = parseInt("70")
+=======
+var name = "Joe"
+var height = "74"
+>>>>>>> 1ae70d186a0ab76ef4b38348e247ee9aad08d376
 
 // Don't worry about this
 module.exports = { name, height
--- other_file.js
+++ other_file.js
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 
 var index = require("./index.js")
 console.log(index.name)
+=======
+console.log(name)
+var index = require("./index.js")
+>>>>>>> 1ae70d186a0ab76ef4b38348e247ee9aad08d376
